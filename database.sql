69961c832b4a4754802df6c721a65326de2684f9
Frederic Mineur <fredericmineur@gmail.com>
2018-07-12 14:58:13 +0100

    new modification Fred 12 July

--- database.sql
+++ database.sql
@@ -2,30 +2,49 @@
 -- occurrences
 
 CREATE TABLE occurrences (
-	id uuid PRIMARY KEY,
-	eventDate text,-- REQUIRED: ISO 8601
-	vernacularName, -- OPTIONAL
-	scientificName -- REQUIRED: the name as found in the paper/herbarium/...
-	scientificNameID -- REQUIRED: from WoRMS
-	identificationRemarks -- OPTIONAL
-	identificationReferences -- OPTIONAL list of references used for the identification
-	occurrenceID -- REQUIRED: globally unique
-	institutionCode -- can be used to compose an occurrence ID: urn:catalog:[institutionCode]:[collectionCode]:[catalogNumber] 
-	collectionCode -- can be used to compose an occurrence ID: urn:catalog:[institutionCode]:[collectionCode]:[catalogNumber] 
-	catalogNumber -- can be used to compose an occurrence ID: urn:catalog:[institutionCode]:[collectionCode]:[catalogNumber] 
-	associatedSequences -- example "GenBank: U34853.1"
-	decimalLongitude numeric NOT NULL, -- REQUIRED
-	decimalLatitude numeric NOT NULL, -- REQUIRED
-	coordinateUncertaintyInMeters numeric, -- OPTIONAL 
+	id_uuid numeric PRIMARY KEY,
+	occurrence_ID text NOT NULL,-- REQUIRED: globally unique (UNIQUE in the occurences table, not in the history table)
+	event_Date text,-- REQUIRED: ISO 8601
+	vernacular_Name text, -- OPTIONAL
+	scientific_Name text NOT NULL, -- REQUIRED: the name as found in the paper/herbarium/...
+	scientific_Name_ID text NOT NULL, -- REQUIRED: from WoRMS
+	identification_Remarks text, -- OPTIONAL
+	identification_References text, -- OPTIONAL list of references used for the identification
+	institution_Code text, -- can be used to compose an occurrence ID: urn:catalog:[institutionCode]:[collectionCode]:[catalogNumber] 
+	collection_Code text, -- can be used to compose an occurrence ID: urn:catalog:[institutionCode]:[collectionCode]:[catalogNumber] 
+	catalog_Number text,-- can be used to compose an occurrence ID: urn:catalog:[institutionCode]:[collectionCode]:[catalogNumber] 
+	associated_Sequences text,-- example "GenBank: U34853.1"
+	decimal_Longitude numeric NOT NULL, -- REQUIRED
+	decimal_Latitude numeric NOT NULL, -- REQUIRED
+	coordinate_Uncertainty_In_Meters numeric, -- OPTIONAL 
 	locality text, -- OPTIONAL
-	locationID text, -- OPTIONAL e.g. marinespecies identifier
-	occurrenceRemarks text, -- OPTIONAL: remarks about occurrence (e.g. stranded on the beach) 
-	occurrenceStatus -- REQUIRED: present/absent
-	basisOfRecord -- REQUIRED: PreservedSpecimen/FossilSpecimen/LivingSpecimen/HumanObservation/MachineObservation
-	associatedReferences -- OPTIONAL: literature references
-	doi -- OPTIONAL: doi for the literature reference
-	associatedMedia -- OPTIONAL: upload images to flickr (upload API) then put the link here OR let people add the url to the image
-	habitat -- vocabulary ???? EMODNet
+	location_ID text, -- OPTIONAL e.g. marine region identifier
+	occurrence_Remarks text, -- OPTIONAL: remarks about occurrence (e.g. stranded on the beach) 
+	occurrence_Status text, -- REQUIRED: present/absent
+	basis_Of_Record text, -- REQUIRED: PreservedSpecimen/FossilSpecimen/LivingSpecimen/HumanObservation/MachineObservation
+	associated_References text, -- OPTIONAL: literature references
+	doi text, -- OPTIONAL: doi for the literature reference
+	associated_Media text, -- OPTIONAL: upload images to flickr (upload API) then put the link here OR let people add the url to the image
+	habitat text,-- vocabulary ???? EMODNet
+    created_by_user numeric NOT NULL,
+	creation_date timestamp NOT NULL,
+	validated_by_user numeric,
+	validation_date timestamp,
+	last_modified_by_user numeric,
+	last_modification_date timestamp
+)
+
+CREATE TABLE users (
+	id numeric PRIMARY KEY,
+	last_name text NOT NULL,
+	first_name text NOT NULL,
+	username text NOT NULL,
+	password text NOT NULL,
+)
+
+
+
+
 )
 
 
