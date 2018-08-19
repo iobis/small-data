0a7d798239625f5b402c7d7d4ac5dfd552e19ad7
Frederic Mineur <fredericmineur@gmail.com>
2018-08-19 01:05:52 +0100

    18/08/19a datafixtures with csv

--- small-data/src/DataFixtures/OccurrencesSpeciesFixtures.php
+++ small-data/src/DataFixtures/OccurrencesSpeciesFixtures.php
@@ -9,10 +9,14 @@ use App\Repository\InputterRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 
 class OccurrencesSpeciesFixtures extends Fixture
 {
+
+    public const SPECIES_REFERENCE = 'species';
+
     //https://symfony.com/doc/master/bundles/DoctrineFixturesBundle/index.html
     private $encoder;
 
@@ -61,38 +65,87 @@ class OccurrencesSpeciesFixtures extends Fixture
 
 
 
+//https://stackoverflow.com/questions/35792244/symfony-doctrine-data-fixture-how-to-handle-large-csv-file
+        $csvSpecies = fopen(dirname(__FILE__).'/species.csv', 'r');
+        $rowCsvSpecies=0;
+//        $species= array();
+        while (!feof($csvSpecies)){
+            $rowCsvSpecies++;
 
+            $lineSpecies = fgetcsv($csvSpecies, [], ';');
+            if($rowCsvSpecies>1) {
+                $species = new Species();
+                $species->setSpeciesNameWorms($lineSpecies[0] );
+                $species->setWormsAphiaId($lineSpecies[1]);
+                $manager->persist($species);
+
+//                $this->addReference('species-'.$rowCsvSpecies, $species[$rowCsvSpecies]);
+                ////!!! the first element of the array is NULL
+
+            }
+        }
+        fclose($csvSpecies);
+        $manager->flush();
+
+
+
+        $speciesRep = $manager->getRepository(Species::class);
+        $speciesForOccurrences = $speciesRep->findBy([]);
         $inputterRep=$manager->getRepository(Inputter::class);
         $inputtersForOccurrences = $inputterRep->findBy([]);
 
-        //associative array of species and AphiaId
-        $arraySpecies = array("494791"=>"Sargassum muticum", "145721"=>"Undaria pinnatifida","137206"=>"Chelonia mydas","123776"=>"Asterias rubens","135304"=>"Chrysaora hysoscella");
 
-        foreach($arraySpecies as $aphiaId => $speciesName) {  //aphiaId is key and speciesName is value
-            $species = new Species();
-            $species->setWormsAphiaId($aphiaId)
-                ->setSpeciesNameWorms($speciesName);
-            $manager->persist($species);
-
-            for($i = 0; $i<= mt_rand (0, 30); $i++){
+        $csvOccurrences = fopen(dirname(__FILE__).'/occurrences.csv', 'r');
+        $rowCsvOccurrences = 0;
+        while (!feof($csvOccurrences)) {
+            $rowCsvOccurrences++;
+            $lineOccurrence = fgetcsv($csvOccurrences, [], ';');
+            if ($rowCsvOccurrences > 1){
                 $occurrence = new Occurrence();
-                //https://stackoverflow.com/questions/25278645/getting-a-random-object-from-an-array-in-php
-                $inputter = $inputtersForOccurrences[array_rand($inputtersForOccurrences)];
-
-                $occurrence->setEventDate($faker->dateTimeBetween('-100 years'))
-                    ->setDecimalLongitude($faker->longitude)
-                    ->setDecimalLatitude($faker->latitude)
-                    ->setOccurrenceCreatedAt($faker->dateTimeBetween('-100 days'))
-                    ->setSpecies($species)
-                    ->setInputter($inputter)
-                //optional
-                    ->setLocality($faker->optional(0.9)->country)
-                    ->setOccurrenceRemarks($faker->optional(0.7)->paragraph(2))
-                    ->setAssociatedMediaUrl($faker->optional(0.3)->imageUrl(320,240));
-
-                $manager->persist($occurrence);
+            foreach ($speciesForOccurrences as $singleSpeciesForOccurrences) {
+                if ($singleSpeciesForOccurrences->getSpeciesNameWorms() == $lineOccurrence[1]) {
+                    $occurrence->setSpecies($singleSpeciesForOccurrences);
+                }
             }
+//                $species = $speciesForOccurrences[array_rand($speciesForOccurrences)];
+//                $occurrence->setSpecies($species);
+
+
+                    $occurrence->setDecimalLatitude($lineOccurrence[6]);
+                    $occurrence->setDecimalLongitude($lineOccurrence[5]);
+                    //https://stackoverflow.com/questions/12447110/php-date-format-remove-time-and-more
+                   $date = ($lineOccurrence[4].'-01-01');
+                    $createDate = new \DateTime($date);
+//                    $dateWithoutTime = $createDate->format('YYYY-MM-DD');
+//                    $occurrence->setEventDate($dateWithoutTime);
+//                https://stackoverflow.com/questions/6238992/converting-string-to-date-and-datetime
+//                        $year = $lineOccurrence[4];
+//                    $dateString = $year.'-01-01';
+//                    $ymd = \DateTime::createFromFormat('Y-m-d', $dateString);
+//                    $occurrence->setEventDate($ymd);
+
+                 //  $occurrence->setEventDate($faker->dateTimeBetween('-100 years'));
+
+
+                    $occurrence->setEventDate($createDate);
+                   $occurrence->setLocality($lineOccurrence[3]);
+                   $occurrence->setOccurrenceRemarks($lineOccurrence[2]);
+
+                    //https://stackoverflow.com/questions/25278645/getting-a-random-object-from-an-array-in-php
+                    $inputter = $inputtersForOccurrences[array_rand($inputtersForOccurrences)];
+                    $occurrence->setInputter($inputter);
+                    $occurrence->setOccurrenceCreatedAt($faker->dateTimeBetween('-100 days'));
+                    $manager->persist($occurrence);
+
+
+
+
         }
-       $manager->flush();
-     }
+
+        }
+        fclose($csvOccurrences);
+        $manager->flush();
+
+
+    }
 }
